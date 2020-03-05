8db8e2bff8215a487bd3428a08cfcb4bf8a9ab81
Evgeny <z_krivonos@mail.ru>
2020-03-05 16:21:57 +0300

    Add files via upload

--- app/apidata/apidata.js
+++ app/apidata/apidata.js
@@ -13,7 +13,7 @@ export default class ApiData {
          * Рекомендуется заменить ссылку в запросе на более надежную
          * Пример файла находится в папке app/lead.json
          */
-        return fetch("https://stat.sitemanage.ru/api/5/lead", {
+        return fetch("https://jspopup.000webhostapp.com/lead.json", {
             methods: 'POST'
         })
          .then(response => response.json())
